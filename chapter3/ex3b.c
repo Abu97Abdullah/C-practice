d7ac1e6cd6e8add100081f8a53d00a210104fe80
Abu <abu97abdullah@gmail.com>
2024-01-21 00:26:21 -0500

    Add Chapter3 exercise solutions

---
+++ chapter3/ex3b.c
@@ -0,0 +1,17 @@
+// Used this to verify what I wrote in ex3.txt for (b)
+#include <stdio.h>
+
+int main(void)
+{
+    int a, b, c, d, e, f;
+
+    scanf("%d -%d -%d", &a, &b, &c);
+
+    printf("\n%d, %d, %d\n", a, b, c);
+
+    scanf("%d-%d-%d", &d, &e, &f);
+
+    printf("\n%d, %d, %d\n", d, e, f);
+
+    return 0;
+}
