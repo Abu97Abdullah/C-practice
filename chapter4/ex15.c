4275f02f01a6bfeb55855470037c3765604848a2
Abu <abu97abdullah@gmail.com>
2024-01-31 00:04:04 -0500

    Add rest of ch4 exercise solutions

---
+++ chapter4/ex15.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i, j;
+
+    i = 1; j = 2;
+    i += j;
+    printf("%d %d\n", i, j); // 3 2
+
+    i = 1; j = 2;
+    i--;
+    printf("%d %d\n", i, j); // 0 2
+
+    i = 1; j = 2;
+    i * j / i;
+    printf("%d %d\n", i, j); // 1 2 (no change)
+
+    i = 1; j = 2;
+    i % ++j;
+    printf("%d %d\n", i, j); // 1 3
+
+    return 0;
+}
